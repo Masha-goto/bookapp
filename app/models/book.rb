03c5da49d2b1898593e5a4d4bcb8167cbc670997
masahiro <oosaki.production@gmail.com>
2024-05-01 16:46:14 +0900

    アイキャッチ設定

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,8 +1,12 @@
 class Book < ApplicationRecord
+	has_one_attached :eyecatch
+
 	validates :title, presence: true
 	validates :title, length: { minimum: 2, maximum: 100 }
 
   validates :content, presence: true
+	validates :content, length: { minimum: 10 }
+  validates :content, uniqueness: true
 
 	belongs_to :user
 	has_many :bookmarks, dependent: :destroy
