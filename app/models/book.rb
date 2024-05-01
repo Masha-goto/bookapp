12c10a5f068b0d9fe21383c0555349849a262e2c
masahiro <oosaki.production@gmail.com>
2024-05-01 09:53:31 +0900

    日本語化、自分の投稿か判定する機能追加

--- app/models/book.rb
+++ app/models/book.rb
@@ -6,6 +6,10 @@ class Book < ApplicationRecord
 
 	belongs_to :user
 
+	def display_created_at
+    I18n.l(self.created_at, format: :default)
+	end
+
 	# def author_name
 	# 	user.display_name
 	# end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   has_many :books, dependent: :destroy
+
+  def has_written?(book)
+    books.exists?(id: book.id)
+  end
 
 	# def display_name
 	# 	profile&.nickname || self.email.split('@').first
 	# end
+
 end
